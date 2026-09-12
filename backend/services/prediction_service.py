3ded2f0c754dc1a73bbc79a230813bef46d46c85
akshysasi <Akshaykunnathsasi@gmail.com>
2026-09-12 16:03:58 +0530

    feat: add prediction history logging for Spark analytics

--- backend/services/prediction_service.py
+++ backend/services/prediction_service.py
@@ -1,13 +1,18 @@
 import os
+import csv
+from datetime import datetime
+
 import joblib
 import pandas as pd
 
-# Get the absolute path to the models folder
+# Get the absolute path to the backend folder
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 MODEL_PATH = os.path.join(BASE_DIR, "models", "crop_model.pkl")
 ENCODER_PATH = os.path.join(BASE_DIR, "models", "label_encoder.pkl")
+DATA_PATH = os.path.join(BASE_DIR, "data", "prediction_history.csv")
 
-# Load the trained model and label encoder only once
+# Load model and label encoder once
 model = joblib.load(MODEL_PATH)
 label_encoder = joblib.load(ENCODER_PATH)
 
@@ -15,7 +20,7 @@ label_encoder = joblib.load(ENCODER_PATH)
 def predict_crop(nitrogen, phosphorus, potassium,
                  temperature, humidity, ph, rainfall):
 
-    # Create input DataFrame with the same feature names used during training
+    # Create input DataFrame
     input_data = pd.DataFrame([{
         "nitrogen": nitrogen,
         "phosphorus": phosphorus,
@@ -31,6 +36,22 @@ def predict_crop(nitrogen, phosphorus, potassium,
 
     # Convert numeric prediction back to crop name
     crop = label_encoder.inverse_transform(prediction)[0]
+
+    # Save prediction to CSV
+    with open(DATA_PATH, "a", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow([
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            nitrogen,
+            phosphorus,
+            potassium,
+            temperature,
+            humidity,
+            ph,
+            rainfall,
+            crop
+        ])
 
     return {
         "recommended_crop": crop
